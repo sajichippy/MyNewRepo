8f875272ccd7d878e9f66cdae97b2e34bbfbe949
HP <HP@192.168.1.41>
2025-03-28 07:36:54 +0530

    new changes in excelread

--- MyNewMavenProject/src/main/java/pack/Excel.java
+++ MyNewMavenProject/src/main/java/pack/Excel.java
@@ -1,41 +1,44 @@
 package pack;
 
 import java.io.FileInputStream;
+
 import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Cell;
-
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Excel {
-	XSSFSheet sh;//Sheet1
 	
-	 public Excel() throws IOException {
-		 FileInputStream f=new FileInputStream("C:\\Users\\HP\\OneDrive\\Desktop\\ExcelRead.xlsx");
-		 XSSFWorkbook wb=new XSSFWorkbook(f);
-		 sh=wb.getSheet("Sheet1");
-	 }
+	XSSFSheet sh;  //sheet1
 	
-	public String readData(int i, int j) {//0,0
-		
-		Row r = sh.getRow(i);
-	   Cell c=r.getCell(j);//0
-		int celltype=c.getCellType();//0 or 1
-		switch(celltype) {//0
+	public Excel() throws IOException  {
+		FileInputStream f = new FileInputStream("C:\\Users\\HP\\OneDrive\\Desktop\\ExcelRead.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(f);
+		sh = wb.getSheet("Sheet1");
+	}
+	
+	public String readData(int i, int j) {  //0 0
+		Row r = sh.getRow(i);  //0
+		Cell c = r.getCell(j);  //0
+		int celltype = c.getCellType();   //0 or 1
+		switch(celltype) {
 		case Cell.CELL_TYPE_NUMERIC:
 		{
-			double d=c.getNumericCellValue();//120
+			double d = c.getNumericCellValue();  //120
 			return String.valueOf(d);
+			
 		}
-		case Cell.CELL_TYPE_STRING:
+		
+		case  Cell.CELL_TYPE_STRING:
 		{
 			return c.getStringCellValue();
 		}
-		}
 		
-		return null;
 		
-	
-}
+		
+		
+	}
+		return null;}
 }
